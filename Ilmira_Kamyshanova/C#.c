ce953d8e5450a50b4d2689b6158ecd8f7a7cd5f0
ilmira <ilmiram625@gmail.com>
2024-08-26 17:24:49 +0300

    new files from Ilmira Kamyshanova

---
+++ Ilmira_Kamyshanova/C#.c
@@ -0,0 +1,18 @@
+# C#
+```
+using System;
+
+public class Greeter {
+    public void Greet() {
+        string[] greetings = {"Hello", "Hola", "Bonjour", "Привет", "こんにちは"};
+        foreach(var greeting in greetings) {
+            Console.WriteLine(greeting);
+        }
+    }
+
+    static void Main(string[] args) {
+        Greeter greeter = new Greeter();
+        greeter.Greet();
+    }
+}
+```
